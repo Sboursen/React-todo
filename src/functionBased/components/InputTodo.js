dc0cc6572dd631e0c140ce1db031110905b774b8
sboursen <dev.sboursen@gmail.com>
2022-03-21 13:15:07 +0100

    Style the react icons using inline style

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -39,7 +39,13 @@ const InputTodo = (props) => {
         onChange={onChange}
       />
       <button className="input-submit">
-        <FaPlusCircle />
+        <FaPlusCircle
+          style={{
+            color: 'darkcyan',
+            fontSize: '20px',
+            marginTop: '2px',
+          }}
+        />
       </button>
     </form>
   );
