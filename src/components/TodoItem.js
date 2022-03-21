ee52044e9954bf83c334ce976afb03718e8274e0
sboursen <dev.sboursen@gmail.com>
2022-03-21 11:20:05 +0100

    Add CSS module styling to the todosItems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import styles from './TodoItem.module.css';
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type="checkbox"
+          className={styles.checkbox}
           checked={this.props.todo.completed}
           onChange={() =>
             this.props.handleChangeProps(this.props.todo.id)
           }
         />
-        {this.props.todo.title}
         <button
           onClick={() =>
             this.props.deleteTodoProps(this.props.todo.id)
@@ -19,6 +19,7 @@ class TodoItem extends React.Component {
         >
           Delete
         </button>
+        {this.props.todo.title}
       </li>
     );
   }
