527b2410ed98642b121eabbffc72ee6ade1c0db6
sboursen <dev.sboursen@gmail.com>
2022-03-21 09:34:18 +0100

    Create todosList component

---
+++ src/components/TodosList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
